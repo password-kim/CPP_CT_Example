adf9fbead5eeafb26fc1d2988f06393bb6e82732
password-kim <galaxykim0611@gmail.com>
2021-12-29 20:03:42 +0900

    영어단어 복구 문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -1,27 +1,31 @@
 #include <stdio.h>
 
 int main() {
+	char a[101], b[101];
 	
-	char a[100];
+	int p = 0;
 	
-	int res=0, cnt=0;
+	gets(a);
 	
-	scanf("%s", &a);
-	
-	for(int i=0; a[i]!='\0'; i++){
-		if(a[i]>=48 && a[i]<=57){
-			res = res * 10 + (a[i] - 48);
+	for(int i = 0; a[i] != '\0'; i++){
+		if(a[i] != 32){
+			//대문자 판별. 
+			if(a[i] >= 65 && a[i] <=90){
+				a[i] = a[i] + 32; // 대문자를 소문자로 변환. 
+				b[p] = a[i]; // 대문자로 변환한 문자를 b배열에 대입. 
+				p++;
+			}
+			else{
+				b[p] = a[i]; // 소문자의 경우 그대로 대입. 
+				p++;
+			}
 		}
 	}
 	
-	printf("%d\n", res);
+	b[p] = '\0'; // 배열의 끝을 알리는 null문자 삽입. 
 	
-	for(int i=1; i < res+1; i++){
-		if(res%i == 0){
-			cnt++;
-		}
-	}
+	printf("%s", b);
 	
-	printf("%d", cnt);
+	return 0;
 
 }
