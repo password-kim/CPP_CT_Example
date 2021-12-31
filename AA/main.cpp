c86f10a5edc9f5b24541c3be271e53d812d83837
password-kim <galaxykim0611@gmail.com>
2021-12-31 12:23:23 +0900

    N자리의 자연수를 쓰는데 가장많이 쓰인 숫자추출문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -1,20 +1,34 @@
 #include <stdio.h>
+#include <climits>
 
-int main() {
-	int n, c = 1, d = 9, sum = 0, res = 0;
-	
-	scanf("%d", &n);
-	
-	while(sum + d < n){
-		sum = sum + d;
-		res = res + (c * d);
-		c++;
-		d = d * 10;
+int main()
+{
+	int n = 0, max = INT_MIN, res = 0, temp;
+
+	char a[100];
+
+	int b[10] = {};
+
+	scanf("%s", &a);
+
+	while (a[n] != '\0')
+	{
+		temp = a[n] - 48;
+		b[temp]++;
+		n++;
 	}
-	
-	res = res + (n - sum) * c;
-	
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (b[i] >= max)
+		{
+			max = b[i];
+			res = i;
+		}
+	}
+
+
 	printf("%d", res);
-	
+
 	return 0;
 }
