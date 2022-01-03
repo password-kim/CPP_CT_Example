840515c165340e1758ced4f90060bd1ddb9ca593
password-kim <galaxykim0611@gmail.com>
2022-01-03 13:05:51 +0900

    N개의 숫자를 뒤집고 소수를 추출하는 문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -1,34 +1,49 @@
 #include <stdio.h>
-#include <climits>
+
+bool isPrime(int x)
+{
+	if(x == 1){
+		return false;
+	}
+	
+	bool flag = true;
+	
+	for(int i = 2; i < x; i++){
+		if(x % i == 0){
+			flag = false;
+			break;
+		}
+	}
+	
+	return flag;
+}
+
+int reverse(int x){
+	int res = 0, tmp;
+	while(x > 0){
+		tmp = x % 10;
+		res = res * 10 + tmp;
+		x = x / 10;
+	}
+	return res;
+}
 
 int main()
 {
-	int n = 0, max = INT_MIN, res = 0, temp;
+	int n, num, i, tmp;
+	scanf("%d", &n);
 
-	char a[100];
-
-	int b[10] = {};
-
-	scanf("%s", &a);
-
-	while (a[n] != '\0')
+	for (int i = 1; i <= n; i++)
 	{
-		temp = a[n] - 48;
-		b[temp]++;
-		n++;
-	}
-
-	for (int i = 0; i < 10; i++)
-	{
-		if (b[i] >= max)
-		{
-			max = b[i];
-			res = i;
+		scanf("%d", &num);
+		tmp = reverse(num);
+		if(isPrime(tmp)){
+			printf("%d ", tmp);
 		}
 	}
 
+	
 
-	printf("%d", res);
 
 	return 0;
 }
