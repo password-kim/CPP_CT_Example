aa311f0b2d78ce5a44705cc06316c938fb9ab5bf
password-kim <galaxykim0611@gmail.com>
2022-01-03 14:15:02 +0900

    1부터 N까지의 소수의 갯수 추출문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -1,49 +1,25 @@
 #include <stdio.h>
 
-bool isPrime(int x)
-{
-	if(x == 1){
-		return false;
-	}
-	
-	bool flag = true;
-	
-	for(int i = 2; i < x; i++){
-		if(x % i == 0){
-			flag = false;
-			break;
-		}
-	}
-	
-	return flag;
-}
-
-int reverse(int x){
-	int res = 0, tmp;
-	while(x > 0){
-		tmp = x % 10;
-		res = res * 10 + tmp;
-		x = x / 10;
-	}
-	return res;
-}
 
 int main()
 {
-	int n, num, i, tmp;
+	int n, flag, cnt = 0;
+	
 	scanf("%d", &n);
-
-	for (int i = 1; i <= n; i++)
-	{
-		scanf("%d", &num);
-		tmp = reverse(num);
-		if(isPrime(tmp)){
-			printf("%d ", tmp);
+	
+	for(int i = 2; i <= n; i++){
+		flag = 1;
+		for(int j = 2; j * j <= i; j++){
+			if(i % j == 0){
+				flag = 0;
+				break;
+			}
+		}
+		if(flag == 1){
+			cnt++;
 		}
 	}
-
+	printf("%d\n", cnt);
 	
-
-
 	return 0;
 }
