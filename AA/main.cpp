07c08808faed3306a2f57ebf1c04a9bded420bcb
password-kim <galaxykim0611@gmail.com>
2022-01-03 16:09:37 +0900

    아나그램(Anagram)문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -3,23 +3,58 @@
 
 int main()
 {
-	int n, flag, cnt = 0;
-	
-	scanf("%d", &n);
-	
-	for(int i = 2; i <= n; i++){
-		flag = 1;
-		for(int j = 2; j * j <= i; j++){
-			if(i % j == 0){
-				flag = 0;
-				break;
-			}
+	char a[100], b[100];
+	int c[52] = {}, d[52] = {}, cnt = 0;
+
+	scanf("%s\n", &a);
+	scanf("%s", &b);
+
+	for (int i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] >= 65 && a[i] <= 90)
+		{
+			int temp = a[i] - 65;
+			c[temp]++;
 		}
-		if(flag == 1){
-			cnt++;
+		else
+		{
+			int temp = a[i] - 71;
+			c[temp]++;
 		}
 	}
-	printf("%d\n", cnt);
-	
+
+	for (int i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] >= 65 && b[i] <= 90)
+		{
+			int temp = b[i] - 65;
+			d[temp]++;
+		}
+		else
+		{
+			int temp = b[i] - 71;
+			d[temp]++;
+		}
+	}
+
+	for (int i = 0; i < 52; i++)
+	{
+		if (c[i] != d[i])
+		{
+			cnt++;
+			break;
+		}
+	}
+
+	if (cnt == 0)
+	{
+		printf("YES");
+	}
+	else
+	{
+		printf("NO");
+	}
+
+
 	return 0;
 }
