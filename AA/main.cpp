50f887fa4b925f4a1b16cff930301655b9706613
password-kim <galaxykim0611@gmail.com>
2022-01-05 15:03:34 +0900

    분노유발자 문제.
    N명의 학생의 앉은키정보를 받고 뒤학생 모두를 가리는 학생수 추출하는 문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -3,24 +3,31 @@
 
 int main()
 {
-	int n, m, val, cnt = 0, max = INT_MIN;
+	int n, cnt = 0, max = INT_MIN;
 
-	scanf("%d %d", &n, &m);
+	int a[101] = {}; // 학생들의 앉은키 정보를 받는 배열 초기화. 
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
 	
-	for(int i = 0; i < n; i++){
-		scanf("%d", &val);
-		if(val > m){
+	// 배열의 끝에서부터 앞으로 가면서 앉은키를 읽음. 
+	for (int i = n - 1; i >= 0; i--)
+	{
+		// 앞에 학생이 현재 가장큰 학생보다 크면 최댓값을 교체하고 cnt를 1증가. 
+		if (a[i] > max) 
+		{
+			max = a[i];
 			cnt++;
-		}
-		else{
-			cnt = 0;
-		}
-		if(cnt > max){
-			max = cnt;
 		}
 	}
 	
-	printf("%d\n", max);
-	
+	// 처음 학생의 앉은키를 받을때 cnt를 1증가시켰으므로 1을 빼줌. 
+	printf("%d\n", cnt - 1);
+
+
 	return 0;
 }
