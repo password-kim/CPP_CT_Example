00a833622728bd4b32827d5d7b0b93db5d0e6ea6
password-kim <galaxykim0611@gmail.com>
2022-01-06 17:29:37 +0900

    가위바위보 문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -1,33 +1,39 @@
 #include <stdio.h>
-#include <climits>
 
 int main()
 {
-	int n, cnt = 0, max = INT_MIN;
-
-	int a[101] = {}; // 학생들의 앉은키 정보를 받는 배열 초기화. 
-
+	int n;
+	
 	scanf("%d", &n);
-
-	for (int i = 0; i < n; i++)
-	{
+	
+	int a[100], b[100];
+	
+	// 1: 가위 2: 바위 3: 보 
+	for(int i = 0; i < n; i++){
 		scanf("%d", &a[i]);
 	}
 	
-	// 배열의 끝에서부터 앞으로 가면서 앉은키를 읽음. 
-	for (int i = n - 1; i >= 0; i--)
-	{
-		// 앞에 학생이 현재 가장큰 학생보다 크면 최댓값을 교체하고 cnt를 1증가. 
-		if (a[i] > max) 
-		{
-			max = a[i];
-			cnt++;
-		}
+	for(int i = 0; i < n; i++){
+		scanf("%d", &b[i]);
 	}
 	
-	// 처음 학생의 앉은키를 받을때 cnt를 1증가시켰으므로 1을 빼줌. 
-	printf("%d\n", cnt - 1);
-
+	for(int i = 0; i < n; i++){
+		if(a[i] == b[i]){
+			printf("D");
+		}
+		else if(a[i] == 1 && b[i] == 3){
+			printf("A");
+		}
+		else if(a[i] == 2 && b[i] == 1){
+			printf("A");
+		}
+		else if(a[i] == 3 && b[i] == 2){
+			printf("A");
+		}
+		else{
+			printf("B");
+		}
+	}
 
 	return 0;
 }
