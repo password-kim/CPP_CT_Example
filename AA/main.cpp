4e167b8254647e5e80a4018ff92e878d2da99a33
password-kim <galaxykim0611@gmail.com>
2022-01-07 18:12:24 +0900

    온도의 최댓값 문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -1,76 +1,44 @@
 #include <stdio.h>
+#include <climits>
 
 int main()
 {
-	int vala = 0, valb = 0;
+	int n, k, max = INT_MIN, sum = 0;
 
-	int a[10], b[10];
-	char c[10];
+	int a[100000] = {};
 
-	for (int i = 0; i < 10; i++)
+	scanf("%d %d", &n, &k);
+
+	for (int i = 0; i < n; i++)
 	{
 		scanf("%d", &a[i]);
 	}
-
-	for (int i = 0; i < 10; i++)
+	
+	// 처음 온도의 합을 구해준다. 
+	for (int i = 0; i < k; i++)
 	{
-		scanf("%d", &b[i]);
+		sum += a[i];
 	}
 
-	for (int i = 0; i < 10; i++)
+	if (sum > max)
 	{
-		if (a[i] > b[i])
+		max = sum;
+	}
+	
+	// 다음 온도의 합을 구하는 과정. 
+	for (int i = 1; i < n - k + 1; i++)
+	{
+		// 이전 처음 온도를 빼주고 새로운 온도를 더해준다. 
+		sum -= a[i - 1];
+		sum += a[i + k - 1];
+
+		if (sum > max)
 		{
-			vala += 3;
-			c[i] = 'A';
-		}
-		else if (a[i] < b[i])
-		{
-			valb += 3;
-			c[i] = 'B';
-		}
-		else
-		{
-			vala++;
-			valb++;
-			c[i] = 'D';
+			max = sum;
 		}
 	}
 
-	printf("%d %d\n", vala, valb);
-
-	if (vala > valb)
-	{
-		printf("A");
-	}
-	else if (vala < valb)
-	{
-		printf("B");
-	}
-	else
-	{
-		for (int i = 9; i >= 0; i--)
-		{
-			if (c[i] == 'A')
-			{
-				printf("A");
-				break;
-			}
-			else if (c[i] == 'B')
-			{
-				printf("B");
-				break;
-			}
-			else
-			{
-				if (i == 0 && c[i] == 'D')
-				{
-					printf("D");
-					break;
-				}
-			}
-		}
-	}
+	printf("%d", max);
 
 	return 0;
 }
