90ad0d8e1cd6a50cb12a8742144e07604389357f
password-kim <galaxykim0611@gmail.com>
2022-01-10 15:43:48 +0900

    석차구하기 문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -1,64 +1,36 @@
 #include <stdio.h>
 #include <algorithm>
+using namespace std;
 
 int main()
 {
-	int n, temp;
+	int n;
 
-	int a[100] = {}, b[100] = {};
-
+	int a[100], res[100] = {};
+	
 	scanf("%d", &n);
 
 	for (int i = 0; i < n; i++)
 	{
 		scanf("%d", &a[i]);
 	}
+	
+	for(int i = 0; i < n; i++){
+		res[i] = 1;
+	}
 
-	for (int i = 0; i < n - 1; i++)
-	{ 
-		if (a[i] > a[i + 1])
-		{
-			temp = a[i] - a[i + 1];
-			// 두 수의 차가 1에서 N-1사이의 숫자면 b배열에 추가하고
-			// 아니라면 NO를 출력하고 프로그램 종료. 
-			if (temp >= 0 && temp <= n - 1)
-			{
-				b[temp] = temp;
-			}  
-			else
-			{
-				printf("NO");
-				exit(0);
-			}
-		}
-		else
-		{
-			temp = a[i + 1] - a[i];
-			if (temp >= 0 && temp <= n - 1)
-			{
-				b[temp] = temp;
-			}
-			else
-			{
-				printf("NO");
-				exit(0);
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(a[i] < a[j]){
+				res[i]++;
 			}
 		}
 	}
-
-	for (int i = 1; i <= n - 1; i++)
-	{
-		// 처음 b배열을 0으로 초기화 했으므로
-		// b배열에 0이있다면 1에서 n - 1의 숫자가 안나왔다는 뜻이므로
-		// NO를 출력하고 프로그램 종료. 
-		if (b[i] == 0)
-		{
-			printf("NO");
-			exit(0);
-		}
+	
+	for(int i = 0; i < n; i++){
+		printf("%d ", res[i]);
 	}
 
-	printf("YES");
 
 
 	return 0;
