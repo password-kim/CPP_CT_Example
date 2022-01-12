9b8092fbe28b305823d413fdd49ad38dc4f9a0cb
password-kim <galaxykim0611@gmail.com>
2022-01-12 14:49:21 +0900

    N!을 소수로 표현하는 문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -1,31 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main()
 {
-	int n, a[10000] = {}, res[10000] = {};
+	int n, a[1000] = {}, sum = 1, temp;
 
 	scanf("%d", &n);
-	
-	for(int i = 0; i < n; i++){
-		scanf("%d", &a[i]);
-	}
-	
-	for(int i = 0; i < n; i++){
-		res[i] = 1;
-	}
-	
-	for(int i = n -1; i >= 0; i--){
-		for(int j = i - 1; j >= 0; j--){
-			if(a[i] <= a[j]){
-				res[i]++;
+
+	for (int i = n; i > 0; i--)
+	{
+		temp = i;
+
+		for (int j = 2; j <= i; j++)
+		{
+			if (temp % j == 0)
+			{
+				while (temp > 1 && temp % j == 0)
+				{
+					a[j]++;
+					temp /= j;
+				}
 			}
 		}
 	}
 	
-	for(int i = 0; i < n; i++){
-		printf("%d ", res[i]);
+	printf("%d! = ", n);
+
+	for (int i = 2; i <= n; i++)
+	{
+		if (a[i] != 0)
+		{
+			printf("%d ", a[i]);
+		}
 	}
-	
-	
+
 	return 0;
 }
