661aefe7569149827808d726901438c15d886e2f
password-kim <galaxykim0611@gmail.com>
2022-02-10 01:26:31 +0900

    탄화수소 질량구하는 문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -1,30 +1,80 @@
 #include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
 
 int main()
 {
-	int n, lt = 1, cur, rt, k = 1, sum = 0;
-	
-	scanf("%d", &n);
-	
-	while(lt != 0){
-		lt = n / (k * 10);
-		cur = (n / k) % 10;
-		rt = n % k;
-		
-		if(cur > 3){
-			sum += (lt + 1) * k;
+	int sum = 0, intc = 0, inth = 0;
+
+	string c, h;
+
+	string a;
+
+	cin >> a;
+
+	for (int i = 0; i < a.size(); i++)
+	{
+		int temp = i;
+		if (a[i] == 67)
+		{
+			if (a[i + 1] == 72)
+			{
+				intc = 12;
+			}
+			else
+			{
+				while (a[temp + 1] >= 48 && a[temp + 1] <= 57)
+				{
+					c += a[temp + 1];
+					temp++;
+				}
+			}
 		}
-		else if(cur < 3){
-			sum += (lt * k);
+
+		if (a[i] == 72)
+		{
+			if (a[i + 1] == NULL)
+			{
+				inth = 1;
+			}
+			else
+			{
+				while (a[temp + 1] >= 48 && a[temp + 1] <= 57)
+				{
+					h += a[temp + 1];
+					temp++;
+				}
+			}
 		}
-		else{
-			sum += (lt * k) + (rt + 1);
-		}
-		
-		k = k * 10;
 	}
-	
-	printf("%d", sum);
+
+	if (intc == 12)
+	{
+		if (inth == 1)
+		{
+			sum = intc + inth;
+		}
+		else
+		{
+			sum = intc + atoi(h.c_str());
+		}
+	}
+	else
+	{
+		if (inth == 1)
+		{
+			sum = atoi(c.c_str()) * 12 + inth;
+		}
+		else
+		{
+			sum = atoi(c.c_str()) * 12 + atoi(h.c_str());
+		}
+	}
+
+	cout << sum;
+
 
 	return 0;
 }
