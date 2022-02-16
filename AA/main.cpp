6e6e03b05e61ccd2562ee732d1d69217c6a19591
password-kim <galaxykim0611@gmail.com>
2022-02-16 14:22:48 +0900

    Special Sort.
    음의 정수는 왼쪽으로 양의 정수는 오른쪽으로 이동시키는 문제. (단, 음의 정수의 순서와 양의 정수의 순서는 바뀌면 안됨.)

--- AA/main.cpp
+++ AA/main.cpp
@@ -5,29 +5,32 @@ using namespace std;
 int main()
 {
 	int n, temp;
-	
+
 	int a[100];
-	
+
 	scanf("%d", &n);
-	
-	for(int i = 0; i < n; i++){
+
+	for (int i = 0; i < n; i++)
+	{
 		scanf("%d", &a[i]);
 	}
-	
-	for(int i = 0; i < n - 1; i++){
-		for(int j = 0; j < n - i - 1; j++){
-			if(a[j] > a[j + 1]){
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if(a[j] > 0 && a[j + 1] < 0){
 				temp = a[j];
 				a[j] = a[j + 1];
 				a[j + 1] = temp;
 			}
 		}
 	}
-	
-	for(int i = 0; i < n; i++){
+
+	for (int i = 0; i < n; i++)
+	{
 		printf("%d ", a[i]);
 	}
-	
 
 	return 0;
 }
