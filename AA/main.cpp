0ef69a4f441af93a333202a402ae0a760d85bb56
password-kim <galaxykim0611@gmail.com>
2022-02-16 13:24:09 +0900

    버블정렬.

--- AA/main.cpp
+++ AA/main.cpp
@@ -4,44 +4,30 @@ using namespace std;
 
 int main()
 {
-	int n, temp, cnt = 0;
-
-	int a[100] = {};
-
+	int n, temp;
+	
+	int a[100];
+	
 	scanf("%d", &n);
-
-	for (int i = 0; i < n; i++)
-	{
+	
+	for(int i = 0; i < n; i++){
 		scanf("%d", &a[i]);
 	}
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (a[j] > a[i])
-			{
+	
+	for(int i = 0; i < n - 1; i++){
+		for(int j = 0; j < n - i - 1; j++){
+			if(a[j] > a[j + 1]){
 				temp = a[j];
-				a[j] = a[i];
-				a[i] = temp;
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
 			}
 		}
 	}
-
-	for (int i = 0; i < n; i++)
-	{
-		if (a[i] > a[i + 1])
-		{
-			cnt++;
-		}
-
-		if (cnt == 2)
-		{
-			printf("%d", a[i + 1]);
-			break;
-		}
+	
+	for(int i = 0; i < n; i++){
+		printf("%d ", a[i]);
 	}
-
+	
 
 	return 0;
 }
