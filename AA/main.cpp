ad2d22a83a077b4997837cb90aaf305238aabb40
password-kim <galaxykim0611@gmail.com>
2022-02-16 16:26:26 +0900

    삽입정렬.

--- AA/main.cpp
+++ AA/main.cpp
@@ -15,15 +15,16 @@ int main()
 		scanf("%d", &a[i]);
 	}
 
-	for (int i = 0; i < n - 1; i++)
-	{
-		for (int j = 0; j < n - i - 1; j++)
-		{
-			if(a[j] > 0 && a[j + 1] < 0){
-				temp = a[j];
-				a[j] = a[j + 1];
-				a[j + 1] = temp;
+	for(int i = 1; i < n; i++){
+		temp = a[i];
+		for(int j = i - 1; j >= 0; j--){
+			if(a[j] > temp){
+				a[j + 1] = a[j];
 			}
+			else{
+				break;
+			}
+			a[j] = temp;
 		}
 	}
 
