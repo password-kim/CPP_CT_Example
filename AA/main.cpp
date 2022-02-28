cffcb747a2742da913f1057d5fc422f610234f90
password-kim <galaxykim0611@gmail.com>
2022-02-28 14:18:48 +0900

    Inversion Sequence.
    첫줄의 n이 주어지고 두번 째 줄에 Inversion Sequence가 주어졌을때 Original Sequence를 구하는 문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -1,40 +1,31 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 using namespace std;
-
-int C[20];
 
 int main()
 {
-	int s, n, a, pos;
+	int n, pos;
 	
-	scanf("%d %d", &s, &n);
+	scanf("%d", &n);
+	
+	vector<int> is(n+1), os(n+1);
 	
 	for(int i = 1; i <= n; i++){
-		scanf("%d", &a);
-		pos = -1;
-		for(int j = 0; j < s; j++){
-			if(C[j] == a){
-				pos = j;
-			}
-		}
-		
-		if(pos == -1){
-			for(int k = s - 1; k >= 1; k--){
-				C[k] = C[k - 1];
-			}
-		}
-		else{
-			for(int l = pos; l >= 1; l--){
-				C[l] = C[l - 1];
-			}
-		}
-		
-		C[0] = a;
+		scanf("%d", &is[i]);
 	}
 	
-	for(int m = 0; m < s; m++){
-		printf("%d ", C[m]);
+	for(int j = n; j >= 1; j--){
+		pos = j;
+		for(int k = 1; k <= is[j]; k++){
+			os[pos] = os[pos + 1];
+			pos++;
+		}
+		os[pos] = j;
+	}
+	
+	for(int m = 1; m <= n; m++){
+		printf("%d ", os[m]);
 	}
 
 	return 0;
