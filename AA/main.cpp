eb2bfd9e24e68cc2a6e670573cbe1f19f2abc7ac
password-kim <galaxykim0611@gmail.com>
2022-03-02 15:51:44 +0900

    두 배열의 교집합
    두 배열이 주어졌을 때 두 배열의 교집합을 오름차순으로 출력하는 문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -4,44 +4,50 @@
 #include <algorithm>
 using namespace std;
 
-int a[101], b[101], c[300];
-
 int main()
 {
-	int n, m, i, p1 = 1, p2 = 1, p3 = 1;
+	int n, m, i, p1 = 0, p2 = 0, p3 = 0;
 
 	scanf("%d", &n);
+	
+	vector<int> a(n);
 
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		scanf("%d", &a[i]);
 	}
 
+	sort(a.begin(), a.end());
+
 	scanf("%d", &m);
 	
-	for (i = 1; i <= m; i++)
+	vector<int> b(m), c(n + m);
+
+	for (i = 0; i < m; i++)
 	{
 		scanf("%d", &b[i]);
 	}
-	
-	while(p1 <= n && p2 <= m){
-		if(a[p1] < b[p2]){
+
+	sort(b.begin(), b.end());
+
+	while (p1 < n && p2 < m)
+	{
+		if (a[p1] == b[p2])
+		{
 			c[p3++] = a[p1++];
+			p2++;
 		}
-		else{
-			c[p3++] = b[p2++];
+		else if (a[p1] < b[p2])
+		{
+			p1++;
 		}
-	}
-	
-	while(p1 <= n){
-		c[p3++] = a[p1++];
-	}
-	
-	while(p2 <= m){
-		c[p3++] = b[p2++];
+		else
+		{
+			p2++;
+		}
 	}
 
-	for (i = 1; i < p3; i++)
+	for (i = 0; i < p3; i++)
 	{
 		printf("%d ", c[i]);
 	}
