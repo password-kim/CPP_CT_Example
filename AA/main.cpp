a014adce95a7109c4cf180733f92ec8372af48f6
password-kim <galaxykim0611@gmail.com>
2022-03-11 19:45:13 +0900

    연속된 자연수의 합
    양의 정수 N이 입력되면 2개 이상의 연속된 자연수의 합으로 정수 N을 표현하는 방법의 가짓수를 출력하는 문제.

--- AA/main.cpp
+++ AA/main.cpp
@@ -6,51 +6,55 @@ using namespace std;
 
 int main()
 {
-	int n, m, i, p1 = 0, p2 = 0, p3 = 0;
+	int n, i, j, sum, tmp, pos, a[1000], cnt = 0;
 
 	scanf("%d", &n);
-	
-	vector<int> a(n);
 
-	for (i = 0; i < n; i++)
+	for (i = n; i >= 1; i--)
 	{
-		scanf("%d", &a[i]);
-	}
-
-	sort(a.begin(), a.end());
-
-	scanf("%d", &m);
-	
-	vector<int> b(m), c(n + m);
-
-	for (i = 0; i < m; i++)
-	{
-		scanf("%d", &b[i]);
-	}
-
-	sort(b.begin(), b.end());
-
-	while (p1 < n && p2 < m)
-	{
-		if (a[p1] == b[p2])
+		tmp = i;
+		pos = 0;
+		sum = 0;
+		while (sum <= n)
 		{
-			c[p3++] = a[p1++];
-			p2++;
-		}
-		else if (a[p1] < b[p2])
-		{
-			p1++;
-		}
-		else
-		{
-			p2++;
+			if (tmp == 0)
+			{
+				break;
+			}
+			a[pos] = tmp;
+			sum += tmp;
+			if (sum < n)
+			{
+				tmp--;
+				pos++;
+			}
+			else if (sum == n)
+			{
+				if (pos >= 1)
+				{
+					cnt++;
+					for (j = pos; j >= 0; j--)
+					{
+						if (j == 0)
+						{
+							printf("%d = %d\n", a[j], n);
+						}
+						else
+						{
+							printf("%d + ", a[j]);
+						}
+					}
+				}
+				break;
+			}
+			else
+			{
+				break;
+			}
 		}
 	}
 
-	for (i = 0; i < p3; i++)
-	{
-		printf("%d ", c[i]);
-	}
+	printf("%d", cnt);
 
 
 	return 0;
