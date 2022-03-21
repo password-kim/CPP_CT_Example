bd067f17a4e12f887df945530e160deee64c2569
password-kim <galaxykim0611@gmail.com>
2022-03-21 20:48:02 +0900

    뮤직비디오 문제
    첫 줄에 자연수 n(곡의 수), m(나눠야하는 DVD의 수)이 주어진다. 다음 줄에는 각 노래의 길이가 분 단위로(자연수)로 주어질때 DVD의 최소 용량 크기를 구하시오.

--- AA/main.cpp
+++ AA/main.cpp
@@ -4,36 +4,49 @@
 #include <algorithm>
 using namespace std;
 
-int main()
-{
-	int n, i, key, lt = 0, rt, mid;
-	
-	scanf("%d %d", &n, &key);
-	
-	vector<int> a(n);
-	
-	for(i = 0; i < n; i++){
-		scanf("%d", &a[i]);
+int a[1001], n;
+
+int Count(int s){
+	int i, cnt = 1, sum = 0;
+	for(i = 1; i <= n; i++){
+		if(sum + a[i] > s){
+			cnt++;
+			sum = a[i];
+		}
+		else{
+			sum += a[i];
+		}
 	}
 	
-	sort(a.begin(), a.end());
+	return cnt;
+}
+
+int main()
+{
+	int m, i, lt = 1, rt = 0, mid, res, max = -2147000000;
 	
-	rt = n - 1;
+	scanf("%d %d", &n, &m);
+	
+	for(i = 1; i <= n; i++){
+		scanf("%d", &a[i]);
+		rt += a[i];
+		if(a[i] > max){
+			max = a[i];
+		}
+	}
 	
 	while(lt <= rt){
 		mid = (lt + rt) / 2;
-		
-		if(a[mid] == key){
-			printf("%d\n", mid + 1);
-			return 0;
-		}
-		else if(a[mid] > key){
-			rt = mid - 1;
+		if(mid >= max && Count(mid) <= m){
+			res = mid;
+			rt = mid -1;
 		}
 		else{
 			lt = mid + 1;
 		}
 	}
+	
+	printf("%d\n", res);
 	
 	return 0;
 }
