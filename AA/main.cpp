f0a6b67dc6a098fd0ff17ce894a851d37c03cc9d
password-kim <galaxykim0611@gmail.com>
2022-03-25 17:26:12 +0900

    봉우리 문제(벡터사용)

--- AA/main.cpp
+++ AA/main.cpp
@@ -4,7 +4,6 @@
 #include <algorithm>
 using namespace std;
 
-int a[52][52];
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 
@@ -14,9 +13,12 @@ int main()
 	
 	scanf("%d", &n);
 	
+	//                      행크기             열크기 
+	vector<vector<int> > map(n + 2, vector<int>(n + 2, 0));
+	
 	for(i = 1; i <= n; i++){
 		for(j = 1; j <= n; j++){
-			scanf("%d", &a[i][j]);
+			scanf("%d", &map[i][j]);
 		}
 	}
 	
@@ -24,7 +26,7 @@ int main()
 		for(j = 1; j <= n; j++){
 			flag = 0;
 			for(k = 0; k < 4; k++){
-				if(a[i][j] <= a[i+dx[k]][j+dy[k]]){
+				if(map[i][j] <= map[i+dx[k]][j+dy[k]]){
 					flag = 1;
 					break;
 				}
