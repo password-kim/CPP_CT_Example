19e31f4c78fca0a974b37dfd26f1863628d96615
password-kim <galaxykim0611@gmail.com>
2022-04-11 19:28:37 +0900

    부분집합(DFS)

--- AA/main.cpp
+++ AA/main.cpp
@@ -5,20 +5,31 @@
 #include <algorithm>
 using namespace std;
 
-void D(int v){
-	if(v > 7) return;
-	else{
-		// printf("%d ", v); <= 전위순회 
-		D(v * 2);
-		// printf("%d ", v); <= 중위순회 
-		D(v * 2 + 1);
-		// printf("%d ", v); <= 후위순회 
+int n, ch[11];
+
+void DFS(int L){
+	int i;
+	if(L == n + 1){
+		for(i = 1; i <= n; i++){
+			if(ch[i] == 1){
+				printf("%d ", i);
+			}
+		}
+		puts("");
 	}
+	else{
+		ch[L] = 1;
+		DFS(L + 1);
+		ch[L] = 0;
+		DFS(L + 1);
+	}
+	
 }
 
 int main()
 {	
-	D(1);
+	scanf("%d", &n);
+	DFS(1);
 	
 	return 0;
 }
