96f50584406c5bf5b294b4629f2a85d5732cc00e
password-kim <galaxykim0611@gmail.com>
2022-04-18 17:57:04 +0900

    경로 탐색(DFS)

--- AA/main.cpp
+++ AA/main.cpp
@@ -5,24 +5,39 @@
 #include <algorithm>
 using namespace std;
 
-int map[51][51];
+int map[30][30], ch[30], cnt = 0, n;
+
+void DFS(int v){
+	int i;
+	if(v == n){
+		cnt++;
+	}
+	else{
+		for(i = 1; i <= n; i++){
+			if(map[v][i] == 1 && ch[i] == 0){
+				ch[i] = 1;
+				DFS(i);
+				ch[i] = 0;
+			}
+		}
+	}
+}
 
 int main()
 {	
-	int n, m, i, j, a, b, c;
+	int m, i, a, b;
 	scanf("%d %d", &n, &m);
 	
 	for(i = 1; i <= m; i++){
-		scanf("%d %d %d", &a, &b, &c);
-		map[a][b] = c;
+		scanf("%d %d", &a, &b);
+		map[a][b] = 1;
 	}
 	
-	for(i = 1; i <= n; i++){
-		for(j = 1; j <= n; j++){
-			printf("%d ", map[i][j]);
-		}
-		printf("\n");
-	}
+	ch[1] = 1;
+	
+	DFS(1);
+	
+	printf("%d\n", cnt);
 	
 	return 0;
 }
