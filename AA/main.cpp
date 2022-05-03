dc24d2d81a13d4fcd7b1301d08b6997190f278ac
password-kim <galaxykim0611@gmail.com>
2022-05-03 14:25:52 +0900

    공주 구하기(조세퍼서: 큐를 이용하는 방법)

--- AA/main.cpp
+++ AA/main.cpp
@@ -6,32 +6,26 @@
 #include <queue>
 using namespace std;
 
-int ch[10001], d[3] = {1, -1, 5};
-
 int main()
 {	
-	int s, e, x, pos, i;
-	scanf("%d %d", &s, &e);
+	int n, k, i;
+	scanf("%d %d", &n, &k);
 	
 	queue<int> Q;
-	
-	ch[s] = 1;
-	Q.push(s);
+	for(i = 1; i <= n; i++){
+		Q.push(i);
+	}
 	
 	while(!Q.empty()){
-		x = Q.front();
+		for(i = 1; i < k; i++){
+			Q.push(Q.front());
+			Q.pop();
+		}
 		Q.pop();
-		for(i = 0; i < 3; i++){
-			pos = x + d[i];
-			if(pos <= 0 || pos > 10000) continue;
-			if(pos == e){
-				printf("%d\n", ch[x]);
-				exit(0);
-			}
-			if(ch[pos] == 0){
-				ch[pos] = ch[x] + 1;
-				Q.push(pos);
-			}
+		
+		if(Q.size() == 1){
+			printf("%d\n", Q.front());
+			Q.pop();
 		}
 	}
 	
