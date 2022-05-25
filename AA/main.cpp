a4873609ff85e2348839fe0d773d6848263855ba
password-kim <galaxykim0611@gmail.com>
2022-05-26 05:53:50 +0900

    순열 구하기(DFS : Depth First Search)

--- AA/main.cpp
+++ AA/main.cpp
@@ -6,60 +6,39 @@
 #include <queue>
 using namespace std;
 
-int dist[101];
+int n, r, cnt = 0;
+int a[20], res[20], ch[20];
 
-struct Edge{
-	int s;
-	int e;
-	int val;
-	Edge(int a, int b, int c){
-		s = a;
-		e = b;
-		val = c;
+int DFS(int L){
+	if(L == r){
+		for(int j = 0; j < L; j++){
+			printf("%d ", res[j]);
+		}
+		cnt++;
+		puts("");
 	}
-};
-
-int main()
-{	
-	vector<Edge> Ed;
-	int i, n, m, a, b, c, j, s, e;
-	scanf("%d %d", &n, &m);
-	
-	for(i = 1; i <= m; i++){
-		scanf("%d %d %d", &a, &b, &c);
-		Ed.push_back(Edge(a, b, c));
-	}
-	
-	for(i = 1; i <= n; i++){
-		dist[i] = 2147000000;
-	}
-	
-	scanf("%d %d", &s, &e);
-	
-	dist[s] = 0;
-	
-	for(i = 1; i < n; i++){
-		for(j = 0; j < Ed.size(); j++){
-			int u = Ed[j].s;
-			int v = Ed[j].e;
-			int w = Ed[j].val;
-			if(dist[u] != 2147000000 && dist[u] + w < dist[v]){
-				dist[v] = dist[u] + w;
+	else{
+		for(int i = 1; i <= n; i++){
+			if(ch[i] == 0){
+				res[L] = a[i];
+				ch[i] = 1;
+				DFS(L + 1);
+				ch[i] = 0;
 			}
 		}
 	}
-	
-	for(j = 0; j < Ed.size(); j++){
-		int u = Ed[j].s;
-		int v = Ed[j].e;
-		int w = Ed[j].val;
-		if(dist[u] != 2147000000 && dist[u] + w < dist[v]){
-			printf("-1\n");
-			exit(0);
-		}
+}
+
+int main()
+{	
+	scanf("%d %d", &n, &r);
+	for(int i = 1; i <= n; i++){
+		scanf("%d", &a[i]);
 	}
 	
-	printf("%d\n", dist[e]);
+	DFS(0);
+	
+	printf("%d\n", cnt);
 	
 	return 0;
 }
