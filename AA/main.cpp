c711b2c7a5052459f903791acc98e4e065748eb7
password-kim <galaxykim0611@gmail.com>
2022-06-22 17:50:21 +0900

    섬나라 아일랜드(BFS 활용)

--- AA/main.cpp
+++ AA/main.cpp
@@ -6,61 +6,53 @@
 #include <queue>
 using namespace std;
 
-vector<pair<int, int> > pz;
-vector<pair<int, int> > hs;
+int n, map[30][30], cnt = 0;
+int dx[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+int dy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
 
-int ch[20], m, res = 2147000000, dis, sum = 0;
+struct Loc{
+	int x;
+	int y;
+	Loc(int a, int b){
+		x = a;
+		y = b;
+	}
+};
 
-void DFS(int L, int s){
-	if(L == m){
-		sum = 0;
-		for(int i = 0; i < hs.size(); i++){
-			int x1 = hs[i].first;
-			int y1 = hs[i].second;
-			dis = 2147000000;
-			for(int j = 0; j < m; j++){
-				int x2 = pz[ch[j]].first;
-				int y2 = pz[ch[j]].second;
-				dis = min(dis, abs(x1 - x2) + abs(y1 - y2));
-			}
-			sum = sum + dis;
-		}
-		if(sum < res){
-			res = sum;
-		}
-	}
-	else{
-		for(int i = s; i < pz.size(); i++){
-			ch[L] = i;
-			DFS(L + 1, i + 1);
-		}
-	}
-	
-}
+queue<Loc> Q;
 
 int main()
 {	
-	ios_base::sync_with_stdio(false);
+	int i, j;
+	scanf("%d", &n);
 	
-	int n, a;
-	cin>>n>>m;
-	for(int i = 1; i <= n; i++){
-		for(int j = 1; j <= n; j++){
-			cin>>a;
-			if(a == 1){
-				hs.push_back(make_pair(i, j));
-			}
-			else{
-				if(a == 2){
-					pz.push_back(make_pair(i, j));
+	for(i = 1; i <= n; i++){
+		for(j = 1; j <= n; j++){
+			scanf("%d", &map[i][j]);
+		}
+	}
+	
+	for(i = 1; i <= n; i++){
+		for(j = 1; j <= n; j++){
+			if(map[i][j] == 1){
+				Q.push(Loc(i, j));
+				map[i][j] = 0;
+				while(!Q.empty()){
+					Loc tmp = Q.front();
+					Q.pop();
+					for(int i =  0; i < 8; i++){
+						if(map[tmp.x + dx[i]][tmp.y + dy[i]] == 1){
+							Q.push(Loc(tmp.x + dx[i], tmp.y + dy[i]));
+							map[tmp.x + dx[i]][tmp.y + dy[i]] = 0;
+						}
+					}
 				}
+				cnt++;
 			}
 		}
 	}
 	
-	DFS(0, 0);
-	
-	cout << res;
+	printf("%d\n", cnt);
 
 	return 0;
 }
