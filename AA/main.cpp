d774d7c01c06a63b9308bd5c60a9b216d3ba19df
password-kim <galaxykim0611@gmail.com>
2022-06-28 21:35:59 +0900

    토마토(BFS 활용)

--- AA/main.cpp
+++ AA/main.cpp
@@ -6,90 +6,66 @@
 #include <queue>
 using namespace std;
 
-int map[21][21], ch[21][21];
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
+int m, n, tom[1010][1010], res = -2147000000, dis[1010][1010];
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {-1, 0, 1, 0};
 
-struct State{
-	int x, y, dis;
-	State(int a, int b, int c){
+struct Loc{
+	int x, y;
+	Loc(int a, int b){
 		x = a;
 		y = b;
-		dis = c;
 	}
-	bool operator<(const State &bb)const{
-		if(dis == bb.dis){
-			if(x == bb.x) return y > bb.y;
-			else return x > bb.x;
-		}
-		else{
-			return dis > bb.dis;
-		}
-	}
-	
 };
 
-struct Lion{
-	int x, y, s, ate;
-	void sizeUp(){
-		ate = 0;
-		s++;
-	}
-};
+queue<Loc> Q;
 
 int main()
 {	
-	int n, i, j, res = 0;
-	priority_queue<State> Q;
-	Lion simba;
-	scanf("%d", &n);
-	for(i = 1; i<= n; i++){
-		for(j = 1; j <= n; j++){
-			scanf("%d", &map[i][j]);
-			if(map[i][j] == 9){
-				simba.x = i;
-				simba.y = j;
-				map[i][j] = 0;
+	scanf("%d %d", &m, &n);
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= m; j++){
+			scanf("%d", &tom[i][j]);
+			if(tom[i][j] == 1){
+				Q.push(Loc(i, j));
 			}
 		}
 	}
-	
-	Q.push(State(simba.x, simba.y, 0));
-	simba.s = 2;
-	simba.ate = 0;
 	
 	while(!Q.empty()){
-		State tmp = Q.top();
+		Loc tmp = Q.front();
 		Q.pop();
-		int x = tmp.x;
-		int y = tmp.y;
-		int z = tmp.dis;
-		if(map[x][y] != 0 && map[x][y] < simba.s){
-			simba.x = x;
-			simba.y = y;
-			simba.ate++;
-			if(simba.ate >= simba.s) simba.sizeUp();
-			map[x][y] = 0;
-			for(i = 1; i <= n; i++){
-				for(j = 1; j <= n; j++){
-					ch[i][j] = 0;
+		for(int i = 0; i < 4; i++){
+			int xx = tmp.x + dx[i];
+			int yy = tmp.y + dy[i];
+			if(tom[xx][yy] == 0){
+				if(xx >= 1 && xx <= n && yy >= 1 && yy <= m){
+					Q.push(Loc(xx, yy));
+					tom[xx][yy] = 1;
+					dis[xx][yy] = dis[tmp.x][tmp.y] + 1;
 				}
 			}
-			while(!Q.empty()){
-				Q.pop();
-			}
-			res = tmp.dis;
-		}
-		for(i = 0; i < 4; i++){
-			int xx = x + dx[i];
-			int yy = y + dy[i];
-			if(xx < 1 || xx > n || yy < 1 || yy > n || map[xx][yy] > simba.s || ch[xx][yy] > 0) continue;
-			Q.push(State(xx, yy, z + 1));
-			ch[xx][yy] = 1;
 		}
 	}
 	
-	printf("%d\n", res);
+	int f = 1;
+	
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= m; j++){
+			if(tom[i][j] == 0) f = 0;
+		}
+	}
+	if(f == 1){
+		for(int i = 1; i <= n; i++){
+			for(int j = 1; j <= m; j++){
+				if(res < dis[i][j]) res = dis[i][j];
+			}
+		}
+		printf("%d\n", res);
+	}
+	else{
+		printf("-1");
+	}
 
 	return 0;
 }
