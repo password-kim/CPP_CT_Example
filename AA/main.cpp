7747320ebb14b9280d8a680948fcb12372ecc6ea
password-kim <galaxykim0611@gmail.com>
2022-07-08 17:01:02 +0900

    네트워크 선 자르기(Bottom-Up)

--- AA/main.cpp
+++ AA/main.cpp
@@ -1,71 +1,22 @@
-#include <stdio.h>
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <algorithm>
-#include <queue>
+#include<bits/stdc++.h>
 using namespace std;
 
-int m, n, tom[1010][1010], res = -2147000000, dis[1010][1010];
-int dx[4] = {0, 1, 0, -1};
-int dy[4] = {-1, 0, 1, 0};
-
-struct Loc{
-	int x, y;
-	Loc(int a, int b){
-		x = a;
-		y = b;
-	}
-};
-
-queue<Loc> Q;
+int dy[50];
 
 int main()
 {	
-	scanf("%d %d", &m, &n);
-	for(int i = 1; i <= n; i++){
-		for(int j = 1; j <= m; j++){
-			scanf("%d", &tom[i][j]);
-			if(tom[i][j] == 1){
-				Q.push(Loc(i, j));
-			}
-		}
+	ios_base::sync_with_stdio(false);
+	int n;
+	cin>>n;
+	
+	dy[1] = 1;
+	dy[2] = 2;
+	
+	for(int i = 3; i <= n; i++){
+		dy[i] = dy[i - 2] + dy[i - 1];
 	}
 	
-	while(!Q.empty()){
-		Loc tmp = Q.front();
-		Q.pop();
-		for(int i = 0; i < 4; i++){
-			int xx = tmp.x + dx[i];
-			int yy = tmp.y + dy[i];
-			if(tom[xx][yy] == 0){
-				if(xx >= 1 && xx <= n && yy >= 1 && yy <= m){
-					Q.push(Loc(xx, yy));
-					tom[xx][yy] = 1;
-					dis[xx][yy] = dis[tmp.x][tmp.y] + 1;
-				}
-			}
-		}
-	}
+	cout<<dy[n];
 	
-	int f = 1;
-	
-	for(int i = 1; i <= n; i++){
-		for(int j = 1; j <= m; j++){
-			if(tom[i][j] == 0) f = 0;
-		}
-	}
-	if(f == 1){
-		for(int i = 1; i <= n; i++){
-			for(int j = 1; j <= m; j++){
-				if(res < dis[i][j]) res = dis[i][j];
-			}
-		}
-		printf("%d\n", res);
-	}
-	else{
-		printf("-1");
-	}
-
 	return 0;
 }
