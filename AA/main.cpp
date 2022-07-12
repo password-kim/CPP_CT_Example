1575481ba3bd4b28f80fbf1f6bebf2517ab328db
password-kim <galaxykim0611@gmail.com>
2022-07-12 15:26:06 +0900

    돌다리 건너기(Bottom-Up)

--- AA/main.cpp
+++ AA/main.cpp
@@ -3,25 +3,20 @@ using namespace std;
 
 int dy[101];
 
-int DFS(int n){
-	if(dy[n] > 0){
-		return dy[n];
-	}
-	if(n == 1 || n == 2){
-		return n;
-	}
-	else{
-		return dy[n] = DFS(n - 1) + DFS(n - 2);
-	}
-}
-
 int main()
 {	
 	ios_base::sync_with_stdio(false);
 	int n;
 	cin>>n;
 	
-	cout << DFS(n) << endl;
+	dy[1] = 1;
+	dy[2] = 2;
+	
+	for(int i = 3; i <= n + 1; i++){
+		dy[i] = dy[i - 1] + dy[i - 2];
+	}
+	
+	cout << dy[n + 1] << endl;
 	
 	return 0;
 }
